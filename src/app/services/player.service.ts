3719404d4c560a91c3c5b2c20e2f4c974fd9fbf5
Joel Dominguez <joeru.desu@gmail.com>
2022-12-09 23:35:50 -0300

    styles all styles

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class PlayerService {
-  private myAppUrl = "https://localhost:7014/";
+  private myAppUrl = environment.backendUrl;
   private myApiUrl = "api/player/";
 
   constructor(private http: HttpClient) {}
